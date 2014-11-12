b011a752c63fc5bc45a2573620070663d43bb64a
Saurfang <ryanjeffreygordon@gmail.com>
2014-11-12 12:22:52 -0500

    Added comments

--- src/qrypt.py
+++ src/qrypt.py
@@ -2,6 +2,7 @@ import algs, util
 from re import findall
 from decimal import Decimal, getcontext
 
+# Returns binary version of 'num'
 def retBin(num):
 	retVal = False
 
@@ -16,6 +17,8 @@ def retBin(num):
 	util.checkFail(retVal)
 	return(retVal)
 
+# Puts the integer values for each character in 'string' (based on chardict)
+# into a string and returns it. - i.e. 'abc' -> 0,1,2 -> '012'
 def retStrVal(string):
 	retVal = False
 
@@ -24,6 +27,8 @@ def retStrVal(string):
 
 		for character in string:
 			charVal = str(util.retInt(character))
+
+			# Adds leading zero if charVal is single digit so all characters can be broken up into two digit pairs.
 			if len(charVal) == 1:
 				charVal = charVal.zfill(2)
 			strVal += charVal
@@ -38,6 +43,7 @@ def retStrVal(string):
 	util.checkFail(retVal)
 	return(retVal)
 
+# Encrypts plaintext 'raw' using given keyCipher
 def encrypt(raw, keyCipher):
 	retVal = False
 
@@ -46,9 +52,10 @@ def encrypt(raw, keyCipher):
 		rawBin = retBin(rawVal)
 
 		ciphVal = rawBin * keyCipher
-		ciphList = findall("..", str(ciphVal)) # Breaks up ciphVal into two digit pairs to utilize entire chardict
+		ciphList = findall("..", str(ciphVal)) # Breaks up 'ciphVal' into two digit pairs to utilize entire chardict
 		ciphText = ""
 
+		# Gets character values for each two digit number in 'ciphList' and adds them to string 'ciphText'
 		for num in ciphList:
 			char = util.retChar(int(num))
 			ciphText += char
@@ -63,28 +70,32 @@ def encrypt(raw, keyCipher):
 	util.checkFail(retVal)
 	return(retVal)
 
+# Decrypts encrypted text 'ciphered' using given keyCipher
 def decrypt(ciphered, keyCipher):
 	retVal = False
 
 	try:
 		ciphVal = ""
 
+		# Gets integer value of each character in 'ciphered' and adds them to string 'ciphVal'
 		for character in ciphered:
 			character = str(character)
 			charVal = str(util.retInt(character))
-			# Keeps charVal two digits
+			# Adds leading zero if charVal is single digit two maintain two digit pairs
 			if len(charVal) == 1:
 				charVal = charVal.zfill(2)
 			ciphVal += charVal
 		ciphVal = int(ciphVal)
 
-		getcontext().prec = 10000000
+		getcontext().prec = 10000000 # Precision of binVal (must be enough to accomodate 100% precision)
 		binVal = str(Decimal(ciphVal) / Decimal(keyCipher))
 		rawVal = str(int(binVal, 2))
 		rawVal = rawVal[1:] # Removes the initial 1 added in retStrVal
+
 		rawList = findall("..", rawVal) # Breaks rawVal into two digit pairs so it may be properly decrypted.
 		rawText = ""
 
+		# Gets character value for each two-digit number in 'rawList' and adds it two 'rawText'
 		for num in rawList:
 			num = int(num)
 			char = util.retChar(num)
